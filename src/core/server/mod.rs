d883f6ad0e604d7e5c977694ee3c46fbf2ac3acb
Aaron P. <19561067+aaronzper@users.noreply.github.com>
2026-05-06 11:51:50 -0400

    feat: Server replying + logging

--- src/core/server/mod.rs
+++ src/core/server/mod.rs
@@ -1,18 +1,58 @@
-use futures::StreamExt;
-use tokio::io::{AsyncRead, AsyncWrite};
+use futures::{SinkExt, StreamExt};
+use std::fmt::Debug;
+use tokio::{
+    io::{AsyncRead, AsyncWrite},
+    sync::mpsc,
+};
 use tokio_util::codec::{FramedRead, FramedWrite};
+use tracing::*;
 
-use crate::protocol::MessageCodec;
+use crate::protocol::{Message, MessageCodec};
 
-pub async fn handle_client<R: AsyncRead + Unpin, W: AsyncWrite + Unpin>(
-    r: R,
-    w: W,
-) {
+#[instrument]
+pub async fn handle_client<R, W>(r: R, w: W)
+where
+    R: AsyncRead + Unpin + Debug,
+    W: AsyncWrite + Unpin + Debug + Send + 'static,
+{
+    let (tx, mut rx) = mpsc::channel(32);
+    let mut writer = FramedWrite::new(w, MessageCodec);
+    tokio::spawn(
+        async move {
+            while let Some(msg) = rx.recv().await {
+                debug!(?msg, "Sending");
+                if let Err(e) = writer.send(msg).await {
+                    warn!(error=?e, "Reply error");
+                }
+            }
+        }
+        .instrument(Span::current()),
+    );
+
     let mut reader = FramedRead::new(r, MessageCodec);
-    let writer = FramedWrite::new(w, MessageCodec);
-
     while let Some(msg) = reader.next().await {
-        println!("{:#?}", msg);
+        let tx = tx.clone();
+        tokio::spawn(
+            async move {
+                let Ok(msg) = msg else {
+                    warn!(error=?msg, "Couldn't decode message");
+                    return;
+                };
+                debug!(?msg, "Received");
+
+                // test reply for now
+                let reply = Message::Error {
+                    request_id: 1,
+                    error_code: crate::protocol::ErrorCode::Invalid,
+                    message: "Not implemented".to_string(),
+                };
+
+                if let Err(e) = tx.send(reply).await {
+                    warn!(error=?e, "Reply error");
+                }
+            }
+            .instrument(Span::current()),
+        );
     }
 
     println!("bye...");
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,14 +1,32 @@
 use libazpfs::server::handle_client;
 use tokio::net::TcpListener;
+use tracing::*;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| {
+                    tracing_subscriber::EnvFilter::new(
+                        if cfg!(debug_assertions) {
+                            "azpfs_server=debug,libazpfs=debug,warn"
+                        } else {
+                            "warn"
+                        },
+                    )
+                }),
+        )
+        .pretty()
+        .init();
+
     let listener = TcpListener::bind("localhost:19310").await?;
+    info!(binding = ?listener.local_addr(), "Server started!");
 
     loop {
-        let (mut sock, _) = listener.accept().await?;
+        let (sock, _) = listener.accept().await?;
         tokio::spawn(async move {
-            let (r, w) = sock.split();
+            let (r, w) = sock.into_split();
             handle_client(r, w).await;
         });
     }
